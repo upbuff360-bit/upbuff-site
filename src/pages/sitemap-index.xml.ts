6b0b4c123aef3f1fa895c5a9d3f273ac0b7025bd
Greeshma <greeshma@cordovacloud.com>
2026-04-24 18:15:07 +0530

    serve sitemap dynamically

---
+++ src/pages/sitemap-index.xml.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+import fs from 'fs';
+import path from 'path';
+
+export const GET: APIRoute = async () => {
+  try {
+    const filePath = path.resolve('./dist/client/sitemap-index.xml');
+    const sitemap = fs.readFileSync(filePath, 'utf-8');
+
+    return new Response(sitemap, {
+      headers: {
+        'Content-Type': 'application/xml',
+      },
+    });
+  } catch (error) {
+    return new Response('Sitemap not found', { status: 404 });
+  }
+};
