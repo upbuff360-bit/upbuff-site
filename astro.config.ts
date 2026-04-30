abd597e78b7d07decbd9bdb8f3a8453875167c32
upbuff360-bit <upbuff360@gmail.com>
2026-04-30 11:28:07 +0530

    sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
 import type { AstroIntegration } from 'astro';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 import astrowind from './vendor/integration';
 
@@ -24,7 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel(),  
   site: 'https://www.upbuff.com',
   devToolbar: {
